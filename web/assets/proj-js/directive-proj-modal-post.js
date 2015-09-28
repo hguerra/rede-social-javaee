fc3106e597a23394d544945c84cf0bc27715d7e4
hguerra <heitorguerrac@gmail.com>
2015-09-28 18:05:05 -0300

    diretiva modal-post

---
+++ web/assets/proj-js/directive-proj-modal-post.js
@@ -0,0 +1,10 @@
+/**
+ * Created by heitor on 28/09/15.
+ */
+app.directive('modalpost', function () {
+    return {
+        restrict: 'EA',
+        templateUrl: "assets/proj-html/modal-post.html"
+    };
+
+});
