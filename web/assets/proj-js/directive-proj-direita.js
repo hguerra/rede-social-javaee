175cf23b46991f6babe332d439137de192ae2e54
hguerra <heitorguerrac@gmail.com>
2015-09-28 17:26:45 -0300

    diretiva coluna direita e esquerda

---
+++ web/assets/proj-js/directive-proj-direita.js
@@ -0,0 +1,10 @@
+/**
+ * Created by heitor on 28/09/15.
+ */
+app.directive('colunadireita', function () {
+    return {
+        restrict: 'EA',
+        templateUrl: "assets/proj-html/coluna-direita.html"
+    };
+
+});
---
+++ web/assets/proj-js/directive-proj-esquerda.js
@@ -0,0 +1,11 @@
+/**
+ * Created by heitor on 28/09/15.
+ */
+
+app.directive('colunaesquerda', function () {
+    return {
+        restrict: 'EA',
+        templateUrl: "assets/proj-html/coluna-esquerda.html"
+    };
+
+});
