47e1c57022107be56f47a43214fc8a272330e971
hguerra <heitorguerrac@gmail.com>
2015-09-29 16:57:18 -0300

    modal mensagens

---
+++ web/assets/proj-js/directive-proj-modal-mensagens.js
@@ -0,0 +1,9 @@
+/**
+ * Created by heitor on 29/09/15.
+ */
+app.directive('modalmensagens', function () {
+    return {
+        restrict: 'EA',
+        templateUrl: "assets/proj-html/modal-mensagens.html"
+    };
+});
