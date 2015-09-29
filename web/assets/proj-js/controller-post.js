f59104d97c840f3bf994f26b22276e5639575c03
hguerra <heitorguerrac@gmail.com>
2015-09-29 15:59:48 -0300

    botao de retweet

--- web/assets/proj-js/controller-post.js
+++ web/assets/proj-js/controller-post.js
@@ -6,8 +6,11 @@ app.controller('PostController', function () {
     /*
      Atributos
      */
+    /*rede-social*/
     post.buscarPost = '';
+    /*facebook*/
     post.facebook = [];
+    /*twitter*/
     post.twitter = [];
     /*Fake*/
     post.feeds = [
@@ -38,26 +41,40 @@ app.controller('PostController', function () {
     post.hashtag = [{'hashtag': '#um'}, {'hashtag': '#dois'}, {'hashtag': '#tres'},
         {'hashtag': '#quatro'}, {'hashtag': '#cinco'}, {'hashtag': '#seis'},
         {'hashtag': '#sete'}, {'hashtag': '#oito'}, {'hashtag': '#nove'}, {'hashtag': '#dez'}];
-    timelineFake = function (){
-        for(i = 1; i <= 10; i++){
+    timelineFake = function () {
+        for (i = 1; i <= 10; i++) {
             post.facebook.push(i);
             post.twitter.push(i);
         }
-    }
+    };
     timelineFake();
     /*
      Metodos
      */
+    /*Rede-social*/
     post.newPost = function () {
         var newTitulo = '@' + post.usuario.nome + ' ' + '28/09/2015';
         post.feeds.unshift({'titulo': newTitulo, 'mensagem': post.mensagem, 'imagem': post.imagem});
         post.titulo = '';
         post.mensagem = '';
         post.imagem = '';
-    }
+    };
     post.remove = function (feed) {
-        var index = post.feeds.indexOf(feed)
+        var index = post.feeds.indexOf(feed);
         post.feeds.splice(index, 1);
+    }
+    /*Facebook*/
+    post.publicar = function () {
+        console.log('ok');
+    }
+    /*Twitter*/
+    post.tweet = function () {
+
+    }
+    post.retweet = function (tweet) {
+        var index = post.twitter.indexOf(tweet);
+        var retweet = post.twitter[index];
+        console.log(retweet);
     }
 });
 
