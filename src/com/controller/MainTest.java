27f105604bdc2c97c890f863df6ea2eb3ad3f304
hguerra <heitorguerrac@gmail.com>
2015-09-30 17:02:23 -0300

    teste reconhecimento gson

--- src/com/controller/MainTest.java
+++ src/com/controller/MainTest.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import com.google.gson.Gson;
 import com.model.PersonData;
 
 /**
@@ -8,6 +9,7 @@ import com.model.PersonData;
 public class MainTest {
     public static void main(String[] args) {
         PersonData p = new PersonData("Heitor", "Carneiro");
-        System.out.println(p.getFirstName());
+        String json = new Gson().toJson(p, PersonData.class);
+        System.out.println(json);
     }
 }
